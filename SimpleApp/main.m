ec4973afb9f5d16b6f1bb7a9c9e5a3371b98f981
Liang Jin <liangjin@Jins-MacBook-Pro.local>
2015-07-19 13:34:16 +0800

    just commit

---
+++ SimpleApp/main.m
@@ -0,0 +1,12 @@
+//
+//  main.m
+//  SimpleApp
+//
+//  Created by Liang Jin on 7/19/15.
+//  Copyright (c) 2015 Jin. All rights reserved.
+//
+
+
+int main(int argc, char * argv[]) {
+    return 0;
+}
